a4905bb3357f3a6b226a178671c44d476f8873bd
LongW4y <57832191+LongW4y@users.noreply.github.com>
2021-12-03 17:21:00 +0100

    no desktop.ini

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 import pathlib
 from os import path
+from datetime import date
+
+def logMessage(messagetype, base, file):
+    LOGFILE = base + 'logs/logs'
+    NEWDIR_code = 'newDir'
+    NEWDIR = 'New directory has been created - '
+    logToday = LOGFILE + str(date.today().strftime("%Y_%m_%d")) + '.txt'
+    if messagetype == NEWDIR_code:
+        if path.isfile(pathlib.Path(logToday)):
+            ff = open(logToday, "a")
+            ff.write(NEWDIR + file)
+        elif not path.isfile(pathlib.Path(logToday)):
+            ff = open(logToday, "x")
+            ff.write(NEWDIR + file)
+        ff.close()
+        return True
 
 
 def createPaths(base, dirs, confile):
@@ -7,6 +23,8 @@ def createPaths(base, dirs, confile):
     for x in dirs:
         pathlib.Path(base + x).mkdir(parents=True, exist_ok=True)
         f.write(str(x) + " - 1\n")
+        print("Directory has been created - " + str(x))
+        logMessage('newDir', base, x)
     f.close()
 
 
@@ -18,11 +36,17 @@ def checkConfig(base, dirs, confile):
 
 
 if __name__ == "__main__":
-    BASEPATH = str(pathlib.Path(__file__).parent.resolve())
+    BASEPATH = str(pathlib.Path(__file__).parent.resolve()) + '/'
     CONFIGFILE = './config.properties'
-    BASEDIRS = ['/backups/blocks', '/backups/images', '/logs']
+    BASEDIRS = ['logs', 'backups/blocks', 'backups/images']
 
     if checkConfig(BASEPATH, BASEDIRS, CONFIGFILE):
         f = open(CONFIGFILE, "r")
-        for x in range(1, 2):
-            print(f.readline())
+        noDirs = []
+        for x in f:
+            myDir = x.split(None, 1)[0]
+            isPresent = int(x[-2:])
+            for y in BASEDIRS:
+                if myDir == x and isPresent == 1:
+                    noDirs.append(myDir)
+        checkConfig(BASEPATH, noDirs, CONFIGFILE)
