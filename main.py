46b5d85f8bb4e21175882d287d91719a97edf0f4
LongW4y <57832191+LongW4y@users.noreply.github.com>
2021-12-05 20:22:37 +0100

    changes

--- main.py
+++ main.py
@@ -3,77 +3,71 @@ from os import path
 from datetime import date
 from datetime import datetime
 
+# Global variables:
+BASEPATH = str(pathlib.Path(__file__).parent.resolve()) + '/'
+BASEDIRS = ('logs', 'backups\\blocks', 'backups\\images',)
+CONFIGFILE = '.\\config.properties'
+LOGFILE = BASEPATH + 'logs/logs_'
+INFO = ' [INFO]'
+ERROR = ' [ERROR]'
 
 def logMessage(messagetype, base, file):
-    LOGFILE = base + 'logs/logs_'
     CURRENTDATE = str(date.today().strftime("%Y_%m_%d"))
     CURRENTTIME = str(datetime.now().strftime('%H_%M_%S_%f')[:-3])
+
     NEWDIR_code = 'newDir'
-    INFO = ' [INFO]'
-    ERROR = ' [ERROR]'
-    NEWDIR = CURRENTTIME + INFO + ' New directory has been created - '
+    NEWDIR_msg = CURRENTTIME + INFO + ' New directory has been created - '
     NONEWDIRS_code = 'presentDir'
-    NONEWDIR = CURRENTTIME + INFO + ' Directory already present - '
+    NONEWDIR_msg = CURRENTTIME + INFO + ' Directory already present - '
+    DIRSPRESENT_code = 'dirsPresent'
+    DIRSPRESENT_msg = CURRENTTIME + INFO + ' All directories needed are present'
+
     logToday = LOGFILE + CURRENTDATE + '.txt'
+
     if messagetype == NEWDIR_code:
-        msg = NEWDIR + file + '\n'
-        print(msg)
-        if path.isfile(pathlib.Path(logToday)):
-            ff = open(logToday, "a")
-            ff.write(msg)
-        elif not path.isfile(pathlib.Path(logToday)):
-            ff = open(logToday, "x")
-            ff.write(NEWDIR + file + '\n')
-        ff.close()
-        return True
+        msg = NEWDIR_msg + file + '\n'
+        writeMessage(msg, logToday)
     elif messagetype == NONEWDIRS_code:
-        msg = NONEWDIR + file + '\n'
-        print(msg)
-        if path.isfile(pathlib.Path(logToday)):
-            ff = open(logToday, "a")
-            ff.write(msg)
-        elif not path.isfile(pathlib.Path(logToday)):
-            ff = open(logToday, "x")
-            ff.write(msg)
-        ff.close()
-        return True
+        msg = NONEWDIR_msg + file + '\n'
+        writeMessage(msg, logToday)
+    elif messagetype == DIRSPRESENT_code:
+        msg = DIRSPRESENT_msg + '\n'
+        writeMessage(msg, logToday)
 
+def writeMessage(msg, logPath):
+    print(msg)
+    if path.isfile(pathlib.Path(logPath)):
+        ff = open(logPath, "a")
+        ff.write(msg)
+    elif not path.isfile(pathlib.Path(logPath)):
+        ff = open(logPath, "x")
+        ff.write(msg)
+    ff.close()
 
-
-def createPaths(base, dirs, confile):
-    f = open("config.properties", "x")
+def createPaths(base, dirs):
     for x in dirs:
         pathlib.Path(base + x).mkdir(parents=True, exist_ok=True)
-        f.write(str(x) + " - 1\n")
-        print("Directory has been created - " + str(x))
         logMessage('newDir', base, x)
-    f.close()
 
 
-def checkConfig(base, dirs, confile):
-    if path.isfile(pathlib.Path(confile)):
-        return True
-    else:
-        createPaths(base, dirs, confile)
+def checkDirs(base, dirs):
+    presentDirs = []
+    if base == BASEPATH:
+        for x in pathlib.Path(base).rglob('*'):
+            if x.is_dir():
+                presentDirs.append(str(x)[len(BASEPATH):])
+    neededDirs = []
+    for x in dirs:
+        if x in presentDirs:
+            logMessage('presentDir', base, x)
+        elif not x in presentDirs:
+            neededDirs.append(x)
+    if neededDirs:
+        createPaths(base, neededDirs)
+    logMessage('dirsPresent', base, True)
 
 
 if __name__ == "__main__":
-    BASEPATH = str(pathlib.Path(__file__).parent.resolve()) + '/'
-    CONFIGFILE = './config.properties'
-    BASEDIRS = ('logs', 'backups/blocks', 'backups/images')
-
-    if checkConfig(BASEPATH, BASEDIRS, CONFIGFILE):
-        f = open(CONFIGFILE, "r")
-        presentDirs = []
-        for x in f:
-            presentDirs.append(x.split("=")[0])
-            neededDirs = []
-        for x in presentDirs:
-            if not x in BASEDIRS:
-                print(presentDirs)
-                neededDirs.append(x)
-            elif x in BASEDIRS:
-                logMessage('presentDir', BASEPATH, x)
-
+    checkDirs(BASEPATH, BASEDIRS)
 
 
