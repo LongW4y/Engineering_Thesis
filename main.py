c1d9e7fb1c5383726df27c27ccdc509aa59b6ba1
LongW4y <57832191+LongW4y@users.noreply.github.com>
2021-12-05 22:31:20 +0100

    file extension check

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ from datetime import datetime
 BASEPATH = str(pathlib.Path(__file__).parent.resolve()) + '/'
 BASEDIRS = ('logs', 'backups\\blocks', 'backups\\images',)
 CONFIGFILE = '.\\config.properties'
+SUPPORTED = ('png', 'jpg', 'jpeg')
 LOGFILE = BASEPATH + 'logs/logs_'
 INFO = ' [INFO]'
 ERROR = ' [ERROR]'
 
-def logMessage(messagetype, base, file):
+def logMessage(messagetype, directory, file):
     CURRENTDATE = str(date.today().strftime("%Y_%m_%d"))
     CURRENTTIME = str(datetime.now().strftime('%H_%M_%S_%f')[:-3])
 
@@ -21,6 +22,14 @@ def logMessage(messagetype, base, file):
     NONEWDIR_msg = CURRENTTIME + INFO + ' Directory already present - '
     DIRSPRESENT_code = 'dirsPresent'
     DIRSPRESENT_msg = CURRENTTIME + INFO + ' All directories needed are present'
+    WAITINPUT_code = 'waitInput'
+    WAITINPUT_msg = CURRENTTIME + INFO + ' Waiting for user input...'
+    WRONGFILEEXT_code = 'wrongFileExt'
+    WRONGFILEEXT_msg = CURRENTTIME + ERROR + ' File with the provided extension is not supported: '
+    NOFILEEXT_code = 'noFileExt'
+    NOFILEEXT_msg = CURRENTTIME + ERROR + ' File provided does not have any file extension defined'
+    CORRFILEEXT_code = 'correctFile'
+    CORRFILEEXT_msg = CURRENTTIME + INFO + ' Provided file extension is supported: '
 
     logToday = LOGFILE + CURRENTDATE + '.txt'
 
@@ -33,22 +42,34 @@ def logMessage(messagetype, base, file):
     elif messagetype == DIRSPRESENT_code:
         msg = DIRSPRESENT_msg + '\n'
         writeMessage(msg, logToday)
+    elif messagetype == WAITINPUT_code:
+        msg = WAITINPUT_msg + '\n'
+        writeMessage(msg, logToday)
+    elif messagetype == WRONGFILEEXT_code:
+        msg = WRONGFILEEXT_msg + file + '\n'
+        writeMessage(msg, logToday)
+    elif messagetype == NOFILEEXT_code:
+        msg = NOFILEEXT_msg + file + '\n'
+        writeMessage(msg, logToday)
+    elif messagetype == CORRFILEEXT_code:
+        msg = CORRFILEEXT_msg + file + '\n'
+        writeMessage(msg, logToday)
 
 def writeMessage(msg, logPath):
-    print(msg)
+    print(msg[:len(msg)-1])
     if path.isfile(pathlib.Path(logPath)):
         ff = open(logPath, "a")
         ff.write(msg)
+        ff.close()
     elif not path.isfile(pathlib.Path(logPath)):
         ff = open(logPath, "x")
         ff.write(msg)
-    ff.close()
+        ff.close()
 
 def createPaths(base, dirs):
     for x in dirs:
         pathlib.Path(base + x).mkdir(parents=True, exist_ok=True)
         logMessage('newDir', base, x)
-
 
 def checkDirs(base, dirs):
     presentDirs = []
@@ -66,8 +87,36 @@ def checkDirs(base, dirs):
         createPaths(base, neededDirs)
     logMessage('dirsPresent', base, True)
 
+def extensionChecker(filePath):
+    fileExt = filePath.split(".")[-1].lower()
+    if fileExt in SUPPORTED:
+        logMessage('correctFile', filePath, fileExt)
+        return True
+    elif fileExt == '':
+        logMessage('noFileExt', filePath, fileExt)
+        print("Supported file extensions are:", end='')
+        for ext in SUPPORTED:
+            print(ext, end='')
+            if ext != SUPPORTED[-1]:
+                print(',', end=' ')
+        return False
+    else:
+        logMessage('wrongFileExt', filePath, fileExt)
+        print("Supported file extensions are: ", end='')
+        for ext in SUPPORTED:
+            print(ext, end='')
+            if ext != SUPPORTED[-1]:
+                print(',', end=' ')
+        return False
+
+def fileInput():
+    logMessage('waitInput', False, False)
+    #    filePath = input("Please put in an absolute path to an image you would like to have replicated:\n")
+    filePath = 'file.png'
+    if extensionChecker(filePath):
+        print("Passed the vibe check!")
+
 
 if __name__ == "__main__":
     checkDirs(BASEPATH, BASEDIRS)
-
-
+    fileInput()
