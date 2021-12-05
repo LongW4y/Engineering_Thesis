a196454909c4c318d277c2778a815d1d300d966e
LongW4y <57832191+LongW4y@users.noreply.github.com>
2021-12-06 00:30:57 +0100

    copying of the file

--- main.py
+++ main.py
@@ -2,58 +2,36 @@ import pathlib
 from os import path
 from datetime import date
 from datetime import datetime
+from csv import reader
+from shutil import copyfile
+import re
 
 # Global variables:
-BASEPATH = str(pathlib.Path(__file__).parent.resolve()) + '/'
-BASEDIRS = ('logs', 'backups\\blocks', 'backups\\images',)
+BASEPATH = str(pathlib.Path(__file__).parent.resolve()) + '\\'
+BASEDIRS = ('logs', 'backups\\blocks', 'backups\\images')
+IMAGESPATH = 'backups\\images\\'
 CONFIGFILE = '.\\config.properties'
+LOGCONFIG = '.\\logs_config.csv'
 SUPPORTED = ('png', 'jpg', 'jpeg')
 LOGFILE = BASEPATH + 'logs/logs_'
-INFO = ' [INFO]'
-ERROR = ' [ERROR]'
 
 def logMessage(messagetype, directory, file):
     CURRENTDATE = str(date.today().strftime("%Y_%m_%d"))
     CURRENTTIME = str(datetime.now().strftime('%H_%M_%S_%f')[:-3])
-
-    NEWDIR_code = 'newDir'
-    NEWDIR_msg = CURRENTTIME + INFO + ' New directory has been created - '
-    NONEWDIRS_code = 'presentDir'
-    NONEWDIR_msg = CURRENTTIME + INFO + ' Directory already present - '
-    DIRSPRESENT_code = 'dirsPresent'
-    DIRSPRESENT_msg = CURRENTTIME + INFO + ' All directories needed are present'
-    WAITINPUT_code = 'waitInput'
-    WAITINPUT_msg = CURRENTTIME + INFO + ' Waiting for user input...'
-    WRONGFILEEXT_code = 'wrongFileExt'
-    WRONGFILEEXT_msg = CURRENTTIME + ERROR + ' File with the provided extension is not supported: '
-    NOFILEEXT_code = 'noFileExt'
-    NOFILEEXT_msg = CURRENTTIME + ERROR + ' File provided does not have any file extension defined'
-    CORRFILEEXT_code = 'correctFile'
-    CORRFILEEXT_msg = CURRENTTIME + INFO + ' Provided file extension is supported: '
-
     logToday = LOGFILE + CURRENTDATE + '.txt'
 
-    if messagetype == NEWDIR_code:
-        msg = NEWDIR_msg + file + '\n'
-        writeMessage(msg, logToday)
-    elif messagetype == NONEWDIRS_code:
-        msg = NONEWDIR_msg + file + '\n'
-        writeMessage(msg, logToday)
-    elif messagetype == DIRSPRESENT_code:
-        msg = DIRSPRESENT_msg + '\n'
-        writeMessage(msg, logToday)
-    elif messagetype == WAITINPUT_code:
-        msg = WAITINPUT_msg + '\n'
-        writeMessage(msg, logToday)
-    elif messagetype == WRONGFILEEXT_code:
-        msg = WRONGFILEEXT_msg + file + '\n'
-        writeMessage(msg, logToday)
-    elif messagetype == NOFILEEXT_code:
-        msg = NOFILEEXT_msg + file + '\n'
-        writeMessage(msg, logToday)
-    elif messagetype == CORRFILEEXT_code:
-        msg = CORRFILEEXT_msg + file + '\n'
-        writeMessage(msg, logToday)
+    with open('logs_config.csv', 'r') as read_obj:
+        next(read_obj)
+        csv_reader = reader(read_obj)
+        for row in csv_reader:
+            if messagetype == row[1]:
+                if row[3] == 'file':
+                    msg = CURRENTTIME + ' [' + row[0] + ']' + row[2] + file + '\n'
+                    writeMessage(msg, logToday)
+                elif row[3] == '':
+                    msg = CURRENTTIME + ' [' + row[0] + ']' + row[2] + '\n'
+                    writeMessage(msg, logToday)
+
 
 def writeMessage(msg, logPath):
     print(msg[:len(msg)-1])
@@ -67,9 +45,13 @@ def writeMessage(msg, logPath):
         ff.close()
 
 def createPaths(base, dirs):
-    for x in dirs:
-        pathlib.Path(base + x).mkdir(parents=True, exist_ok=True)
-        logMessage('newDir', base, x)
+    if type(dirs) == list:
+        for x in dirs:
+            pathlib.Path(base + x).mkdir(parents=True, exist_ok=True)
+            logMessage('newDir', base, x)
+    elif type(dirs) == str:
+        pathlib.Path(base + dirs).mkdir(parents=True, exist_ok=True)
+        logMessage('newDir', base, dirs)
 
 def checkDirs(base, dirs):
     presentDirs = []
@@ -108,15 +90,30 @@ def extensionChecker(filePath):
             if ext != SUPPORTED[-1]:
                 print(',', end=' ')
         return False
+def copyFile(filePath):
+    delimPlace = len(str(re.split('/|\\\\', filePath)[-1])) + 1
+    if filePath[-delimPlace] == '/':
+        delim = '/'
+        newDir = str(datetime.now().strftime('%Y%m%d_%H%M%S%f')[2:-4])
+        createPaths(BASEPATH + IMAGESPATH, newDir)
+        extensionChecker(filePath)
+        copyfile(filePath, BASEPATH + IMAGESPATH + newDir + delim + filePath.split(delim)[-1])
+    elif filePath[-delimPlace] == '\\':
+        delim = '\\'
+        newDir = str(datetime.now().strftime('%Y%m%d_%H%M%S%f')[2:-4])
+        createPaths(BASEPATH + IMAGESPATH, newDir)
+        extensionChecker(filePath)
+        copyfile(filePath, BASEPATH + IMAGESPATH + newDir + delim + filePath.split(delim)[-1])
 
 def fileInput():
     logMessage('waitInput', False, False)
-    #    filePath = input("Please put in an absolute path to an image you would like to have replicated:\n")
-    filePath = 'file.png'
-    if extensionChecker(filePath):
-        print("Passed the vibe check!")
-
+    filePath = input("Please put in an absolute path to an image you would like to have replicated:\n")
+#    filePath = "C:\\Users\\longw\\Desktop\\G Drive\\Praca inżynierska\\Engineering_Thesis\\test images\\Standard aspect ratio\\FHD\\4k-retro-80s-wallpaper-fhd-1920x1080.jpg"
+#    filePath = 'C:/Users/longw/Desktop/G Drive/Praca inżynierska/Engineering_Thesis/test images/Standard aspect ratio/FHD/4k-retro-80s-wallpaper-fhd-1920x1080.jpg'
+    copyFile(filePath)
 
 if __name__ == "__main__":
     checkDirs(BASEPATH, BASEDIRS)
     fileInput()
+
+
